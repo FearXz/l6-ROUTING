c114e89603f2e8ce715463357f6583fbcbda09e0
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-10 15:24:16 +0100

    asd

--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 function Poster(props) {
   return (
     <Col className={props.responsive}>
-      <img src={props.poster} className=" img-fluid" alt="Media0" />
+      <Link to={`/`}>
+        <img src={props.poster} className=" img-fluid" alt="Media0" />
+      </Link>
     </Col>
   );
 }
